eb1852258e98d52bb2b22412604c4ef411fa1961
788007 <788007@users.noreply.github.com>
2016-09-01 08:50:09 -0700

    Add files via upload

---
+++ ballWorld/BallRunner.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class BallRunner here.
+ * 
+ * @Laurel Woods 
+ * @activity 1
+ */
+public class BallRunner
+{
+    public  static void run() {
+        BallWorld bw = new BallWorld();
+        bw.BallWorld(int width, int height);
+    }
+}
