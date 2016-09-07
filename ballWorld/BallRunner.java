45fd0414dd14a16c12413f75a67221ff02b7bcdc
788007 <788007@users.noreply.github.com>
2016-09-07 08:33:26 -0700

    Add files via upload

--- ballWorld/BallRunner.java
+++ ballWorld/BallRunner.java
@@ -1,14 +1,21 @@
 
 /**
- * Write a description of class BallRunner here.
- * 
  * @Laurel Woods 
  * @activity 1
  */
 public class BallRunner
 {
     public  static void run() {
-        BallWorld bw = new BallWorld();
-        bw.BallWorld(int width, int height);
+        BallWorld ballWorld = new BallWorld(500, 500);
+        TGPoint startPoint = new TGPoint (0, 0);
+        BallBot ballBot = new BallBot(ballWorld, startPoint, 0, 25);
+        
+        while(ballBot.canMoveForward(ballWorld)) {
+            if (ballBot.canMoveForward(ballWorld)){
+                ballBot.moveForward();
+            }else {
+                ballBot.setHeading(
+        }
+      
     }
 }
