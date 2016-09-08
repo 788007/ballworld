9a50b10a31dd1cead39ad47587bb0261ff69b927
788007 <788007@users.noreply.github.com>
2016-09-08 08:49:32 -0700

    Add files via upload

--- ballWorld/BallRunner.java
+++ ballWorld/BallRunner.java
@@ -5,17 +5,37 @@
  */
 public class BallRunner
 {
-    public  static void run() {
+    BallWorld ballWorld;
+    TGPoint entrancePoint;
+    BallBot[] ballBotArray;
+    
+    public BallRunner(BallWorld ballWorld, TGPoint entrancePoint, int ballBotArrayLength){
+        
+        BallBot[] ballBotArray = new BallBot[ballBotArrayLength - 1];
+    }
+    
+    public int findFreeBallBotIndex() {
+        for (int i = 0; i < ballBotArray.length; i ++) {
+            if (ballBotArray[i] == null) {
+                return i;
+            } 
+            
+        }
+        return ballBotArray.length;
+    }
+
+    
+    public static void activity1() {
         BallWorld ballWorld = new BallWorld(500, 500);
         TGPoint startPoint = new TGPoint (0, 0);
         BallBot ballBot = new BallBot(ballWorld, startPoint, 0, 25);
         
-        while(ballBot.canMoveForward(ballWorld)) {
+        while(1 > 0) {
             if (ballBot.canMoveForward(ballWorld)){
                 ballBot.moveForward();
             }else {
-                ballBot.setHeading(
+                ballBot.setHeading((ballBot.getHeading()+120)%360);
+            }
         }
-      
     }
 }
