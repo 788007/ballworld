bcb70758ab55357c29ffa80c327217f9ad1dc81e
788007 <788007@users.noreply.github.com>
2016-09-16 08:51:32 -0700

    Add files via upload

--- ballWorld/BallRunner.java
+++ ballWorld/BallRunner.java
@@ -5,11 +5,9 @@
  */
 public class BallRunner
 {
-    private BallWorld ballWorld;
-    private TGPoint entrancePoint;
-    private BallBot[] ballBotArray;
-    private ballBotArray;
-    
+    static BallBot[] ballBotArray;
+    BallWorld ballWorld;
+    TGPoint entrancePoint;
     public BallRunner(BallWorld ballWorld, TGPoint entrancePoint, int ballBotArrayLength){
         ballWorld = ballWorld;
         entrancePoint = entrancePoint;
@@ -24,23 +22,54 @@ public class BallRunner
         return ballBotArray.length;
     }
     public static void run(){
-        BallWorld ballWorld = new BallWorld(300, 300);
+        BallWorld ballWorld = new BallWorld(500, 500);
         TGPoint entrancePoint = new TGPoint (0, 0);
         int ballBotArrayLength = 10;
-        BallRunner ball = new BallRunner(ballWorld, entrancePoint, ballBotArrayLength);
-        while (1 > 0) {
-            int freeBallBotIndex = ball.findFreeBallBotIndex();
-            if (freeBallBotIndex < ballBotArrayLength) {
-                BallBot ballBot = new BallBot(ballWorld, entrancePoint, (int)(Math.random()*360),10);
+        int ballBotRadius;
+        
+        BallRunner ballObject = new BallRunner(ballWorld, entrancePoint, ballBotArrayLength);
+        for (int j = 0; j < ballBotArray.length; j++) {
+            int freeBallBotIndex = ballObject.findFreeBallBotIndex();
+            if (freeBallBotIndex < ballBotArray.length) {
+                BallBot ballBot = new BallBot(ballWorld, entrancePoint, (int)(Math.random()*360), ballBotRadius);
                 ballBotArray[freeBallBotIndex] = ballBot;
             }
         }
+        while (1 > 0) {
+            for (int index = 0; index < ballBotArray.length; index++) {
+                if (ballBotArray[index] != null ){
+                    if (ballBotArray[index].canMoveForward(ballWorld) == true){
+                        ballBotArray[index].moveForward();
+                   }else {
+                       ballBotArray[index].setHeading((int)(Math.random()*360));
+                       ballBotArray[index].moveForward();
+                   }
+
+                }
+            }
+        }
     }
+    public double distanceBetweenPoints(TGPoint point1, TGPoint point2) {
+        double xDiff = point1.x - point2.x;
+        double yDiff = point1.y - point2.y;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+    public boolean entranceClear(){
+        for (int k = 0; k < ballBotArray.length; k++) {
+            if (ballBotArray[k] != null){
+                if (ballBotArray[k].distanceBetweenPoints(entrancePoint, ballBotArray[k].getPoint()) < 2 * ballBotArray[k].getRadius()){
+                    return false;
+                } else {
+                    return true; 
+                }
+            }
+        }
+    }
+    
     public static void activity1() {
         BallWorld ballWorld = new BallWorld(500, 500);
         TGPoint startPoint = new TGPoint (0, 0);
         BallBot ballBot = new BallBot(ballWorld, startPoint, 0, 25);
-        
         while(1 > 0) {
             if (ballBot.canMoveForward(ballWorld)){
                 ballBot.moveForward();
